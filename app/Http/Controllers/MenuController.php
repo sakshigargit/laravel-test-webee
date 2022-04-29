3ba3c7d11f3fc824421745f90c9a8167fe9c56f9
Sakshi Garg <sakshigarg045@gmail.com>
2022-04-30 04:12:05 +0530

    Added changes for the menu test case

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -28,73 +28,21 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        return response()->json([
-            [
-                "id"=> 1,
-                "name"=> "All events",
-                "url"=> "/events",
-                "parent_id"=> null,
-                "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                "children"=> [
-                    [
-                        "id"=> 2,
-                        "name"=> "Laracon",
-                        "url"=> "/events/laracon",
-                        "parent_id"=> 1,
-                        "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                        "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                        "children"=> [
-                            [
-                                "id"=> 3,
-                                "name"=> "Illuminate your knowledge of the laravel code base",
-                                "url"=> "/events/laracon/workshops/illuminate",
-                                "parent_id"=> 2,
-                                "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "children"=> []
-                            ],
-                            [
-                                "id"=> 4,
-                                "name"=> "The new Eloquent - load more with less",
-                                "url"=> "/events/laracon/workshops/eloquent",
-                                "parent_id"=> 2,
-                                "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "children"=> []
-                            ]
-                        ]
-                    ],
-                    [
-                        "id"=> 5,
-                        "name"=> "Reactcon",
-                        "url"=> "/events/reactcon",
-                        "parent_id"=> 1,
-                        "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                        "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                        "children"=> [
-                            [
-                                "id"=> 6,
-                                "name"=> "#NoClass pure functional programming",
-                                "url"=> "/events/reactcon/workshops/noclass",
-                                "parent_id"=> 5,
-                                "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "children"=> []
-                            ],
-                            [
-                                "id"=> 7,
-                                "name"=> "Navigating the function jungle",
-                                "url"=> "/events/reactcon/workshops/jungle",
-                                "parent_id"=> 5,
-                                "created_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "updated_at"=> "2021-04-27T15=>35=>15.000000Z",
-                                "children"=> []
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]);
+        $menuItems = MenuItem::orderBy('parent_id', 'desc')->get()->toArray();
+        $menu = [];
+        $children = [];
+        foreach ($menuItems as $menuItem) {
+            if ($menuItem['parent_id'] == null) {
+                $menuItem['children'] = !empty($children[$menuItem['id']]) ? $children[$menuItem['id']] : [];
+                $menu[] = $menuItem;
+            } else {
+                $menuItem['children'] = !empty($children[$menuItem['id']]) ? $children[$menuItem['id']] : [];
+                $children[$menuItem['parent_id']][] = $menuItem;
+                if (!empty($children[$menuItem['id']])) {
+                    unset($children[$menuItem['id']]);
+                }
+            }
+        }
+        return response()->json($menu);
     }
 }
